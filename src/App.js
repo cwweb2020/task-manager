94814627df49199112795aa357d1ced8ad398d2e
claudio <bmx347@gmail.com>
2022-09-17 16:00:14 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './style.css'
 
-function App() {
+
+const App = () => {
+ 
+ const tasks = [
+  {id: 1, text: 'Doctors appointment', day: 'Feb 5th at 2:30pm', done: false, note: 'Take the dog with me'},
+  {id: 2, text: 'Meeting at school', day: 'Feb 6th at 1:30pm', done: false, note: 'take document'},
+  {id: 3, text: 'Food shopping', day: 'Feb 5th at 2:30pm', done: false, note: 'dont forget cc'},
+  ]
+  const [changedTask, setChangedTask] = React.useState(tasks)
+
+  const handleCompletedTasks = (id) => {
+  //  console.log(id);
+    const newTasks = changedTask.map((task) => task.id === id ? {...task, done: !task.done} : task)
+    setChangedTask(newTasks)
+  }
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>React App</h1>
+      <div>
+      <ul>
+        {changedTask.map((task) => (
+          <li key={task.id}><h3 style={{color: task.done ? '#f443369e' : '#3f51b5', textDecoration: task.done ? 'line-through' : ''}}>{task.text}</h3> 
+          {task.day}  <br />  <br /> Note: {task.note}    <br />  <br />      
+          
+       <button
+        onClick={() => { handleCompletedTasks(task.id) }}
+       >
+          completed
+       </button> 
+      </li>
+        ))}
+
+      </ul>
+
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
