2bd3556686aa2692a970ee4fc291b645dec678f1
claudio <bmx347@gmail.com>
2022-09-18 02:04:12 -0300

    third commit

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import './style.css'
 const App = () => {
  
  const tasks = [
-  {id: 1, text: 'Doctors appointment', day: 'Feb 5th at 2:30pm', done: false, note: 'Take the dog with me'},
+  {id: 1, text: "Doctor's appointment", day: 'Feb 5th at 2:30pm', done: false, note: 'Take the dog with me'},
   {id: 2, text: 'Meeting at school', day: 'Feb 6th at 1:30pm', done: false, note: 'take document'},
   {id: 3, text: 'Food shopping', day: 'Feb 5th at 2:30pm', done: false, note: 'dont forget cc'},
   {id: 4, text: 'Meeting with the team', day: 'Feb 6th at 1:30pm', done: false, note: 'take the document'},
   {id: 5, text: 'Dentist', day: 'Feb 5th at 2:30pm', done: false, note: "Take the X ray"},
+  {id: 6, text: 'Meeting with the boss', day: 'Feb 6th at 1:30pm', done: false, note: 'No comment'},
   ]
   const [changedTask, setChangedTask] = React.useState(tasks)
   const [search, setSearch] = React.useState('')
@@ -24,11 +25,16 @@ const App = () => {
     setSearch(e.target.value)
    // setChangedTask(tasks.filter((task) => task.text.toLowerCase().includes(search.toLowerCase())))
   }
+   
+  const handleDelete = (id) => {
+    const newTasks = changedTask.filter((task) => task.id !== id)
+    setChangedTask(newTasks)
+  }
 
   
   return (
     <>
-      <h1>Task list</h1>
+      <h1 style={{padding: '0 35px'}}>Task list</h1>
       <div style={{padding: '0 35px'}}>
           <label>Seach task</label> <br /> 
           <input type="text"
@@ -47,12 +53,17 @@ const App = () => {
           //  backgroundColor: task.done ? '#8080804d' : '',
             }}>{task.text}</h3> 
           {task.day}  <br />  <br /> Note: {task.note}    <br />  <br />      
-          
-       <button
-        onClick={() => { handleCompletedTasks(task.id) }}
-       >
-          completed
-       </button> 
+        <span style={{display: 'flex', justifyContent: 'space-between'}}>
+            <button
+              onClick={() => { handleCompletedTasks(task.id) }}
+            >
+                completed
+            </button> 
+            <button style={{background: 'red'}} onClick={() => handleDelete(task.id)}>
+              delete
+            </button>
+
+        </span>  
       </li>
         ))}
 
